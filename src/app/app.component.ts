c394d62a003ee51e910dd3d11f5db20e52dec814
Juan Antonio Flores <pcsijflo@upc.edu.pe>
2024-09-17 15:07:45 -0500

    refactor: update AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,51 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { OnInit, ViewChild } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from "@angular/material/divider";
+import { MatListModule } from "@angular/material/list";
+import { BreakpointObserver } from "@angular/cdk/layout";
 import { TranslateService } from "@ngx-translate/core";
 import { LanguageSwitcherComponent } from "./public/pages/language-switcher/language-switcher.component";
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatButtonModule, MatIconModule, LanguageSwitcherComponent],
+  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatButtonModule, MatIconModule,
+    MatSidenavModule, MatDividerModule, MatListModule, LanguageSwitcherComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'daos-learning-center';
+
+  @ViewChild(MatSidenav, {static: true}) sidenav!: MatSidenav;
   options = [
-    { path: '/home', title: 'Home'},
-    { path: '/learning/students', title: 'Students'},
-    {path:'/about', title: 'About'}
+    { icon: 'home', path: '/home', title: 'Home'},
+    { icon: 'person', path: '/learning/students', title: 'Students'},
+    { icon: 'info', path:'/about', title: 'About'}
   ];
 
-  constructor(translate: TranslateService) {
+  constructor(private translate: TranslateService, private observer: BreakpointObserver) {
     translate.setDefaultLang('en');
     translate.use('en');
+  }
+
+  ngOnInit(): void {
+    this.observer.observe(['(max-width: 1280px)']) // Observa el ancho de la pantalla
+      .subscribe((response) => {  // Se suscribe a los cambios en el ancho de la pantalla
+        if (response.matches) { // Si el ancho de la pantalla es menor a 1280px
+          this.sidenav.mode = 'over'; // Se despliega sobre el contenido
+          this.sidenav.close(); // Se cierra
+        } else {
+          this.sidenav.mode = 'side'; // Se despliega al lado del contenido
+          this.sidenav.open();  // Se abre
+        }
+      });
   }
 }
